6364714ffc7096b687a1fde3d1d2cb7f64a8dd00
kirbloz <>
2021-05-13 10:58:49 +0200

    css inclusion didnt work bc i was working from another folder

--- fetchSuperuser/showUser.inc.php
+++ fetchSuperuser/showUser.inc.php
@@ -5,7 +5,49 @@ o almeno in parte perchè così faccio include e basta
 
 forse
 */
-    @include_once('../php/header.php');
+
+@include_once('session.inc.php');
+@include_once('php/session.inc.php');
+@include_once('../php/session.inc.php');
+@session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyLAN</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="shortcut icon" href="/images/fav.ico"/>
+</head>
+<body>
+	<div class="color-white bg-div">
+		
+		<div class="main-header color-lightb">
+			<h1><a href="index.php">Easy LAN PORTAL</a></h1>
+			<ul class="navlinks">
+				<!--<li><a href="#">Info</a></li>-->
+				
+					<?php
+					if(checkActive()){
+						if($_SERVER['REQUEST_URI'] != '/baisiniProject/areaUtente.php')
+							echo '<li><a href="areaUtente.php">Area Utente</a></li>';
+							echo '<li><a href="php/logout.inc.php?isUser=true">Logout</a></li>';
+					}else if(checkActiveSuper()){
+						if($_SERVER['REQUEST_URI'] != '/baisiniProject/areaSuperutente.php')
+							echo '<li><a href="areaSuperutente.php">Area Utente</a></li>';
+							echo '<li><a href="php/logout.inc.php?isUser=false">Logout</a></li>';
+					}else{
+						echo '<li><a id="login-button" href="login.php">Login</a></li>';
+						echo '<li><a href="signup.php">Registrati</a></li>';
+					}
+					?>
+				
+			</ul>
+		</div>
+<?php
     if(!checkActiveSuper())
         header('location:../superlogin.php?error=nosession'); //session
     @include_once('../php/function.inc.php');
