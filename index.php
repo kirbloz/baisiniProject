58846d8c52eaf1b43bbc83cbd1be5168a1e8e12d
kirbloz <>
2021-05-17 22:46:37 +0200

    working on homepage look

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 			include('php/header.php')
 		?>
 		<!--<img class="index-bg" src="images/bg.jpg" alt="image">-->
-		<div class="wrapper centered">
+		<!--<div class="wrapper centered">
 			<br><br>
-			<div class="container">
+			
 				<div class="row">
 					<div class="col-md-6">
 						<div class="p-3 border bg-light">Benvenuto sul portale Web di EASY LAN</div>
@@ -13,18 +13,42 @@
 						<div class="p-3 border bg-light">Buona permanenza</div>
 					</div>
 				</div>
+		</div>-->
+		
+		<div id="slides" class="carousel slide" data-ride="carousel">
+			<ul class="carousel-indicatiors">
+				<li data-target="#slides" data-slide-to="0" class="active"></li>
+				<li data-target="#slides" data-slide-to="1"></li>
+				<li data-target="#slides" data-slide-to="2"></li>
+			</ul>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="images/bg.jpg">
+					<div class="carousel-caption">
+					<h1 class="display-2">Easy LAN</h1>
+					<h3>Il tuo fornitore di connettivit&agrave;</h3>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="images/table.jpg">
+				</div>
+				<div class="carousel-item">
+					<img src="images/cables.png">
+				</div>
+			</div>
 		</div>
-
+		<br>
+		<br>
 		<!-- <div class="ads"></div> -->
-		<div> SCOPRI DI PI&Ugrave;
+		<div class="container centered"><div> <h1>SCOPRI DI PI&Ugrave;</h1>
 		<hr>
 		</div>
 
-		<div>
+		<div class="">
 			<ul>
-				<li>Cosa facciamo?</li>
+				<li><a href="images/CLIL.pptx" download>Cosa facciamo?</li>
 				<li>Che cosa installiamo?</li>
-				<li>Curiosit&agrave; e approfondimenti</li>
+				<li>Curiosit&agrave; e approfondimenti</a></li>
 			</ul>
 		</div>
 
