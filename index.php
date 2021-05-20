4903b2e586a9785dd8c0dde9e83d8213bb543f43
kirbloz <>
2021-05-20 19:46:11 +0200

    working on graphic styling for the homepage

--- index.php
+++ index.php
@@ -1,63 +1,108 @@
-		<?php
-			include('php/header.php')
-		?>
-		<!--<img class="index-bg" src="images/bg.jpg" alt="image">-->
-		<!--<div class="wrapper centered">
-			<br><br>
-			
-				<div class="row">
-					<div class="col-md-6">
-						<div class="p-3 border bg-light">Benvenuto sul portale Web di EASY LAN</div>
-					</div>
-					<div class="col-md-6">
-						<div class="p-3 border bg-light">Buona permanenza</div>
-					</div>
-				</div>
-		</div>-->
-		
-		<div id="slides" class="carousel slide" data-ride="carousel">
-			<ul class="carousel-indicatiors">
-				<li data-target="#slides" data-slide-to="0" class="active"></li>
-				<li data-target="#slides" data-slide-to="1"></li>
-				<li data-target="#slides" data-slide-to="2"></li>
-			</ul>
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img src="images/bg.jpg">
-					<div class="carousel-caption">
-					<h1 class="display-2">Easy LAN</h1>
-					<h3>Il tuo fornitore di connettivit&agrave;</h3>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<img src="images/table.jpg">
-				</div>
-				<div class="carousel-item">
-					<img src="images/cables.png">
-				</div>
+<?php
+include('php/header.php')
+?>
+<!--<img class="index-bg" src="images/bg.jpg" alt="image">-->
+<!--<div class="wrapper centered">
+	<br><br>
+	
+		<div class="row">
+			<div class="col-md-6">
+				<div class="p-3 border bg-light">Benvenuto sul portale Web di EASY LAN</div>
+			</div>
+			<div class="col-md-6">
+				<div class="p-3 border bg-light">Buona permanenza</div>
 			</div>
 		</div>
-		<br>
-		<br>
-		<!-- <div class="ads"></div> -->
-		<div class="container centered"><div> <h1>SCOPRI DI PI&Ugrave;</h1>
-		<hr>
-		</div>
+</div>-->
 
-		<div class="">
+
+<!-- slider -->
+<div id="slides" class="carousel slide" data-ride="carousel">
+	<ul class="carousel-indicatiors">
+		<li data-target="#slides" data-slide-to="0" class="active"></li>
+		<li data-target="#slides" data-slide-to="1"></li>
+		<li data-target="#slides" data-slide-to="2"></li>
+	</ul>
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="images/bg.jpg">
+			<div class="carousel-caption">
+				<h1 class="display-2">Easy LAN</h1>
+				<h3>Il tuo fornitore di connettivit&agrave;</h3>
+				<button type="button" class="btn btn-outline-light btn-lg">Iniziamo</button>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img src="images/table.jpg">
+		</div>
+		<div class="carousel-item">
+			<img src="images/cables.png">
+		</div>
+	</div>
+</div>
+
+<!-- jumbotron -->
+<div class="container-fluid">
+<div class="row jumbotron">
+	<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+		<p class="lead">Un portale online per accedere ai diversi servizi legati al mercato della connettivit&agrave; offerti dall'azienda EasyLAN </p>
+	</div>
+	<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+		<a href="#"><button type="button" class="btn btn-outline-secondary btn-lg">Leggi</button></a>
+	</div>
+</div>
+</div>
+<!-- -->
+<div class="container-fluid">
+
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h3 class="display-4">Scopri di pi&ugrave;</h3>
+		</div>
+		<hr>
+		<div class="col-12">
 			<ul>
 				<li><a href="images/CLIL.pptx" download>Cosa facciamo?</li>
 				<li>Che cosa installiamo?</li>
 				<li>Curiosit&agrave; e approfondimenti</a></li>
 			</ul>
 		</div>
-
-
-		<footer>
-			<p id="privacy-link">
-				<a href="https://www.iubenda.com/privacy-policy/27774228" rel="noreferrer nofollow" target="_blank">Privacy Policy</a> - <a href="#" role="button" class="iubenda-advertising-preferences-link">Personalizza tracciamento pubblicitario</a>
-			</p>
-		</footer>
 	</div>
+</div>
+<!--footer-->
+
+<footer>
+<div class="container-fluid padding">
+<div class="row text-center">
+	<div class="col-md-4">
+		<hr class="light">
+		<b>EASYLAN</b>
+		<hr class="light">
+		<p>324-092-2939</p>
+		<p>easylanproject@gmail.com</p>
+		<p>Olivelli Putelli</p>
+	</div>
+	<div class="col-md-4">
+		<hr class="light">
+		<p>Orari</p>
+		<hr class="light">
+		<p>Settembre-Giugno</p>
+		<p>08:00 - 14:00</p>
+	</div>
+	<div class="col-md-4">
+		<hr class="light">
+		<p>Le nostre sedi</p>
+		<hr class="light">
+		<p>Darfo</p>
+		<p>Brescia</p>
+	</div>
+</div>
+</div>
+	<div id="privacy-link" class="col-12 text-center">
+	<hr class="light-100">
+		<a href="https://www.iubenda.com/privacy-policy/27774228" rel="noreferrer nofollow" target="_blank"> &copy;Privacy Policy</a> - <a href="#" role="button" class="iubenda-advertising-preferences-link">Personalizza tracciamento pubblicitario</a>
+	</div>
+</footer>
+
 </body>
 </html>
--- php/header.php
+++ php/header.php
@@ -14,7 +14,7 @@
     <title>EasyLAN</title>
 	<!-- bootstrap -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="shortcut icon" href="/images/fav.ico"/>
 </head>
