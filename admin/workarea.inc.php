073392e441afcd861d917c601d16e5a1dff6722c
kirbloz <>
2021-05-22 01:23:34 +0200

    created file

--- admin/workarea.inc.php
+++ admin/workarea.inc.php
@@ -1,28 +1,5 @@
 <?php
-/*
-require('../php/function.inc.php');
-require_once('../php/header.php');
-require_once('adminfun.inc.php');
 
-if (!checkActiveSuper())
-    header('location:superlogin.php?error=nosession'); //session
-//salvo il power level
-
-$utente = generateSuperuserOBJ(session_id());
-
-?>
-<div class="wrapper user-area">
-    <div class='color-lightb user-nav'>
-        <ul>
-            <li><?php
-                if (isset($_GET['matricola']))
-                    echo "<a href='superuserfetch.php?select=techs'>Torna indietro</a>";
-                else if (isset($_GET['idUser']))
-                    echo "<a href='superuserfetch.php?select=users'>Torna indietro</a>";
-                ?></li>
-        </ul>
-    </div>
-    <?php*/
 require_once('adminfun.inc.php');
 if (isset($_POST)) {
     var_dump($_POST);
