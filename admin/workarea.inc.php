4de52082e150ce7e63b354f68ffbe6976ff07256
kirbloz <>
2021-05-22 23:12:18 +0200

    structured the file's logic

--- admin/workarea.inc.php
+++ admin/workarea.inc.php
@@ -2,17 +2,14 @@
 
 require_once('adminfun.inc.php');
 if (isset($_POST)) {
-
-    if (!isset($_POST) == 'submit')
+    if (!isset($_POST['submit']) == 'Aggiungi')
         header('location:../superuserfetch.php?select=works');
-    var_dump($_POST);
     saveWorkDB($_POST);
 } else {
     //probabilmente l'url è stato manomesso, rispedisco indietro
     header('location:../superuserfetch.php?select=works');
 }
 
-echo "work aggiunto";
-//header('refresh: 5; url=../superuserfetch.php?select=works&error=noerror');
+//echo "work aggiunto";
+header('location:../superuserfetch.php?select=works&error=noerror');
 
-echo "<a href='../superuserfetch.php?select=works&error=noerror'>back</a>";
